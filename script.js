8e5aeb578300499bbf3dc1e8fbf0afd6b9e61b40
Elizabeth Bey <ebey04@gmail.com>
2025-10-27 17:16:12 -0400

    feat: create function for basic operation of calculator

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+let num1;
+let num2;
+let operator; 
+
+function operate(n1, n2, op) {
+    if(op === "+") {
+       return add(n1, n2);
+    }
+    else if(op === "-") {
+       return subtract(n1, n2);
+    }
+    else if(op === "x") {
+        return multiply(n1, n2);
+    }
+    else {
+       return  division(n1, n2);
+    }
+}
